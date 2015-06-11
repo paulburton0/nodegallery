032854bf1e30f3f5867b0b5fa31eb406f9b5cf56
Paul Burton <paulburton0@gmail.com>
2015-06-11 19:53:22 -0400

    Added video section to router; created video view.

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,24 @@ var router = express.Router();
 
 var imageDir = '/home/pburton/imagesx';
 
+function getBreadcrumbs(pathname){
+    var pathArray = pathname.split('/');
+    var breadcrumbArray = [];
+    for(i = 1; i < pathArray.length; i++){
+        var crumb = {};
+        var crumbArray = pathArray.slice(1, i);
+        crumb.name = crumbArray.slice(-1).toString();
+        crumb.path = '/' + crumbArray.join('/');
+        breadcrumbArray.push(crumb);
+    }
+    breadcrumbArray[0] = {name: 'root', path: '/'};
+    return breadcrumbArray;
+}
+
     /* GET home page. */
 router.get(/\/.*/, function(req, res, next) {
     var pathname = url.parse(req.originalUrl, true).pathname;
-    if(/\.(jpe?g|png|bmp|gif)$/.test(pathname)){
+    if(/\.(jpe?g|png|bmp|gif|webm)$/.test(pathname)){
         next();
     }
     var start = url.parse(req.originalUrl, true).query.start;
@@ -18,16 +32,26 @@ router.get(/\/.*/, function(req, res, next) {
         start = 0;
     }
     dirlist.getList(absPath, pathname, start, function(err, list){
-        res.render('index', {title: pathname, content: list, start: start, pathname: pathname});
+        res.render('index', {title: pathname, content: list, start: start, pathname: pathname, breadcrumbs: getBreadcrumbs(pathname)});
     });
 
+}, function(req, res, next){
+    var pathname = url.parse(req.originalUrl, true).pathname;
+    if(/\.webm$/.test(pathname)){
+        next();
+    }
+    var start = url.parse(req.originalUrl, true).query.start;
+    if(start == undefined){ 
+        start = 0;
+    }
+    res.render('image', {title: pathname, image: path.join('/imagesx', pathname), pathname: pathname, start: start, breadcrumbs: getBreadcrumbs(pathname)});
 }, function(req, res, next){
     var pathname = url.parse(req.originalUrl, true).pathname;
     var start = url.parse(req.originalUrl, true).query.start;
     if(start == undefined){ 
         start = 0;
     }
-    res.render('image', {title: pathname, image: path.join('/imagesx', pathname), pathname: pathname, start: start});
+    res.render('video', {title: pathname, webm: path.join('/imagesx', pathname), pathname: pathname, start: start, breadcrumbs: getBreadcrumbs(pathname)});
 });
 
 module.exports = router;
