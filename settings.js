d0e4bf1cbfe9e09eb8bff61424b2e558e0dc2b59
Paul Burton <paulburton0@gmail.com>
2015-08-07 15:37:14 -0400

    Added useAuth to settings.

--- settings.js
+++ settings.js
@@ -2,5 +2,6 @@ module.exports = { port : '3000',
                    sslKey : 'ssl/ssl-cert-snakeoil.key',
                    sslCert : 'ssl/ssl-cert-snakeoil.pem',
                    useHttps : true,
-                   imageDirectory : '/home/pburton/imagesx'
+                   imageDirectory : '/home/pburton/imagesx',
+                   useAuth : true
                  }
